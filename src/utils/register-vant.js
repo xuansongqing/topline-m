133b7d4569652a630413fbbf88286a2bef81c7cd
xuansongqing <1519511850@qq.com>
2020-01-09 10:50:58 +0800

    登陆页面结构

--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 
 // 加载
-import { Button } from 'vant'
+import {
+  Button,
+  NavBar,
+  Cell, CellGroup,
+  Field
+} from 'vant'
 // 注册
 Vue.use(Button)
+  .use(NavBar)
+  .use(Cell)
+  .use(CellGroup)
+  .use(Field)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="login-page">登陆页面</div>
+  <div class="login-page">
+    <!-- 导航栏 -->
+    <van-nav-bar title="标题"/>
+    <!-- 登陆表单 -->
+    <van-cell-group>
+      <van-field required clearable label="手机号" placeholder="请输入手机号"/>
+      <van-field label="验证码" placeholder="请输入验证码" required>
+      <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <div>
+      <van-button type="info">信息按钮</van-button>
+    </div>
+  </div>
 </template>
 
 <script>
