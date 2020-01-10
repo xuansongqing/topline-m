36add0abaa9b6c3c96e668306f78f0ec9bb7a87c
xuansongqing <1519511850@qq.com>
2020-01-10 21:16:50 +0800

    表单验证-初始配置

--- src/main.js
+++ src/main.js
@@ -4,12 +4,13 @@ import router from './router'
 import store from './store'
 // 动态设置 REM 基准值（html 标签的字体大小）
 import 'amfe-flexible'
-
 // 注册Vant组件
 import './utils/register-vant'
 // 加载全局样式
 // 注意该文件放到第三方样式之后
 import './styles/index.less'
+// 注册表单验证
+import './utils/validation.js'
 Vue.config.productionTip = false
 
 new Vue({
